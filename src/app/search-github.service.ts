236c466f0d2d31d11dcb18f8ddda234da97bd07a
Yommie Samora <samora.y@adzumi.co.ke>
2018-04-28 07:06:37 +0300

    Saturday Morning. Let's start!

--- src/app/search-github.service.ts
+++ src/app/search-github.service.ts
@@ -9,20 +9,24 @@ export class SearchGithubService {
 	user:User;
 
   	constructor(private http: HttpClient) { 
-  		this.user = new User("",0);
+  		this.user = new User("",0,"","");
   	}
 
 	getUserData(username: string){
 
 		interface ApiResponse{
 	        bio:string,
-	        public_repos:number
+	        public_repos:number,
+	        login:string,
+	        avatar_url:string
 	    }
 
 	    let promise =new Promise((resolve,reject)=>{
 	        this.http.get<ApiResponse>("https://api.github.com/users/" + username).toPromise().then(response=>{
 	            
 	            this.user.bio=response.bio;
+	            this.user.avatar_url=response.avatar_url;
+	            this.user.login=response.login;
 	            this.user.public_repos=response.public_repos;
 
 	            resolve()
--- src/app/user.ts
+++ src/app/user.ts
@@ -1,4 +1,4 @@
 export class User {
 
-	constructor(public bio: string, public public_repos: number){}
+	constructor(public bio: string, public public_repos: number, public login:string, public avatar_url:string){}
 }
